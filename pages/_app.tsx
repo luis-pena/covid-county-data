3aa869ae02db413893df3013068c60e60418cfca
Luis Pena <helloluiis@gmail.com>
2022-03-07 17:30:23 -0800

    update background to grey by overriding theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { createTheme, ThemeProvider, useTheme } from "@mui/material";
 import { FunctionComponent } from "react";
 import { Provider } from "react-redux";
 
@@ -7,18 +8,21 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { useTheme } from "@mui/material";
 
-export default function App({ Component }: { Component: FunctionComponent }) {
+export default function MyApp({ Component }: { Component: FunctionComponent }) {
   const theme = useTheme();
+  const customTheme = createTheme(theme, {
+    palette: {
+      background: {
+        default: theme.palette.grey[100],
+      },
+    },
+  });
   return (
     <Provider store={store}>
-      <Component />
-      <style jsx global>{`
-        body {
-          background-color: ${theme.palette.grey[100]};
-        }
-      `}</style>
+      <ThemeProvider theme={customTheme}>
+        <Component />
+      </ThemeProvider>
     </Provider>
   );
 }
